c482b93b93d4c717437abe1fefe3bf1029cac123
Tunahan Dogan <piyealar@gmail.com>
2023-05-02 19:01:25 +0300

    responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -51,16 +51,16 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto my-6">
-      <header className="border-b pb-6 mb-6 flex items-center justify-between">
+    <div className="container mx-auto my-6 px-4">
+      <header className="border-b pb-6 mb-6 text-center md:text-left">
         <div>
-          <h6 className="flex items-center gap-x-2 text-xl font-bold">
+          <h6 className="flex items-center justify-center gap-x-2 text-xl font-bold">
             SUMMARY
             <span className="bg-yellow-500 rounded-md px-4 py-1 text-black">
               APP
             </span>
           </h6>
-          <p className="text-base font-medium text-zinc-600 mt-2">
+          <p className="text-base font-medium text-zinc-600 mt-2 text-center">
             Girdiğiniz metni özetleyen bir araç. Developed By Tunahan
             (Jagshem#1948)
           </p>
@@ -68,20 +68,21 @@ export default function Home() {
       </header>
       <form
         onSubmit={submitHandle}
-        className="flex gap-x-4 items-center justify-center"
+        className="flex flex-wrap gap-x-4 gap-y-4 justify-center items-center"
       >
         <input
+          type="password"
           value={apiKey}
           onChange={(e) => setApiKey(e.target.value)}
           placeholder="OpenAI API Anahtarı"
-          className="w-[400px] bg-zinc-100 outline-none focus:bg-zinc-300 p-4 rounded-md text-[15px] font-medium"
+          className="w-full md:w-[400px] bg-zinc-100 outline-none focus:bg-zinc-300 p-4 rounded-md text-[15px] font-medium"
         />
         <textarea
           value={query}
           id="input-textarea"
           onChange={((e) => setQuery(e.target.value), handleTextAreaChange)}
           placeholder={loading ? 'Özetleniyor...' : 'Metni buraya yazınız...'}
-          className="w-[400px] bg-zinc-100 outline-none focus:bg-zinc-300 p-4 rounded-md text-[15px] font-medium resize-none max-h-[225px] overflow-y-auto loading:cursor-pointer-events-none"
+          className="w-full md:w-[400px] bg-zinc-100 outline-none focus:bg-zinc-300 p-4 rounded-md text-[15px] font-medium resize-none max-h-[225px] overflow-y-auto loading:cursor-pointer-events-none"
         />
         <button
           disabled={!query || loading}
@@ -99,24 +100,26 @@ export default function Home() {
         </button>
       </form>
       <div className="container mx-auto my-6">
-        {/* header, form, and button code remains the same */}
         {summaries.length > 0 && (
           <div className="mt-6">
             <h3 className="text-lg font-bold mb-4">Özetler</h3>
             {summaries.map((item, index) => (
-              <div className="flex mb-3" key={item.key}>
-                <div className="w-1/2 p-4 bg-zinc-100 rounded-md mr-3">
+              <div
+                className="flex mb-3 flex-col md:flex-row gap-y-4 md:gap-y-0"
+                key={item.key}
+              >
+                <div className="w-full md:w-1/2 p-4 bg-zinc-100 rounded-md">
                   <h4 className="text-lg font-bold mb-3">{item.name}</h4>
                   <p>{item.summary}</p>
                 </div>
-                <div className="w-1/2 p-4 bg-zinc-100 rounded-md ml-3">
+                <div className="w-full md:w-1/2 p-4 bg-zinc-100 rounded-md">
                   <h4 className="text-lg font-bold mb-3">GİRDİĞİNİZ METİN</h4>
                   <p>{item.originalText}</p>
                 </div>
                 <button
                   type="button"
                   onClick={() => handleRemoveSummary(index)}
-                  className="h-10 px-4 ml-3 rounded-md bg-red-500 text-white font-bold hover:bg-red-400"
+                  className="h-10 px-4 md:ml-3 rounded-md bg-red-500 text-white font-bold hover:bg-red-400 self-start"
                 >
                   X
                 </button>
